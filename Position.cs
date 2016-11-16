b534fa4c399ed2ea4c1236f006183a7ec2a8c8c6
priclavv <kacper.drejer@gmail.com>
2016-11-16 20:24:54 +0100

    Implementacja metody Position.IsPositionInRange

--- Position.cs
+++ Position.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Config;
 namespace Checkers
 {
     public struct Position
     {
         public int x, y;
         public bool IsPositionInRange()
-        { throw new NotImplementedException(); }
+        {
+            if (x < 0 || y < 0 || x >= Cfg.board_size || y >= Cfg.board_size)
+                return false;
+            return true;
+        }
     }
 }
