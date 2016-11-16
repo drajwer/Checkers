cf7fcd3b763bd6faa755c859fe3aedae276255cd
ZoltanDC <damianc1d@interia.pl>
2016-11-16 16:27:31 +0100

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers
+{
+    public enum Color { WHITE, BLACK };
+
+    class Game
+    {
+        protected enum State { GAME, GAMEOVER, NOGAME };
+
+        protected State gameState;
+        protected CheckerBoard board;
+        protected Player player1,
+            player2,
+            currentPlayer;
+
+        public Game()
+        {
+            InitGame();
+        }
+
+        public void InitGame()
+        {
+
+            throw new NotImplementedException();
+
+            // kolor dla gracza wylosowac
+            // ustawic graczy, plansze stworzyc
+
+        }
+
+        public void Run()
+        {
+            while (gameState == State.GAME)
+            {
+                if (IsGameOver())
+                    gameState = State.GAMEOVER;
+
+                currentPlayer.Turn(board);
+                //zmienia bierzacego gracza
+            }
+        }
+
+        private bool IsGameOver()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        private void AnnounceWinning()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
